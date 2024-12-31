963508964e26485f2ef467ac75bcc18159e92e1a
sahilbhardwaj23 <143410453+sahilbhardwaj23@users.noreply.github.com>
2024-12-31 10:27:48 +0530

    commit virtual assistant

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="multilingual assistant",
+    version="0.0.0",
+    author="Sahil",
+    author_email="Sahil2322002@gmail.com",
+    packages=find_packages(),
+    # install_requires=["SpeechRecognition","pipwin","pyaudio","gTTS","google-generativeai","python-dotenv","streamlit"]
+)
